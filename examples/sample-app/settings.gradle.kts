e346bff5f54f89cb66d553312eca9a9eb4cacc12
PJ <ipriyanshujain@gmail.com>
2026-04-19 12:51:39 +0700

    chore(sample-app): add KMP root gradle config

---
+++ examples/sample-app/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "uatu-sample"
+
+include(":composeApp")
