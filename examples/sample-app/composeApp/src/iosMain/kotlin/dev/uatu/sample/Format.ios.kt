7b74b86cbd41762544838d3abdaea7e8abcde41d
PJ <ipriyanshujain@gmail.com>
2026-04-19 12:53:17 +0700

    feat(sample-app): add shared number and date formatting

---
+++ examples/sample-app/composeApp/src/iosMain/kotlin/dev/uatu/sample/Format.ios.kt
@@ -0,0 +1,23 @@
+package dev.uatu.sample
+
+import platform.Foundation.NSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSLocale
+import platform.Foundation.dateWithTimeIntervalSince1970
+
+private val DATE_FMT = NSDateFormatter().apply {
+    locale = NSLocale("en_US_POSIX")
+    dateFormat = "MMM d, h:mm a"
+}
+
+private val CLOCK_FMT = NSDateFormatter().apply {
+    locale = NSLocale("en_US_POSIX")
+    dateFormat = "HH:mm"
+}
+
+private fun date(epochMillis: Long): NSDate =
+    NSDate.dateWithTimeIntervalSince1970(epochMillis / 1000.0)
+
+actual fun formatDate(epochMillis: Long): String = DATE_FMT.stringFromDate(date(epochMillis))
+
+actual fun formatClock(epochMillis: Long): String = CLOCK_FMT.stringFromDate(date(epochMillis))
